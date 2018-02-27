e9d4537fe2a169e33abb6e84a31d34ed946ed322
Vadim <vadik_mig@mail.ru>
2018-02-27 22:31:12 +0300

    make solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var strArray = str.split("");
+  var config  = bracketsConfig;
+  var arr = [];
+  while (strArray.length > 0) {
+  for (var i = 0; i < config.length; i++) {
+      var brackets = config[i];
+    if (brackets[0] == brackets[1] && brackets[1] == strArray[0]) {
+        if (brackets[0] == arr[arr.length-1]) {
+            arr.pop();
+            strArray.shift();
+        } else if (brackets[0] !== arr[arr.length-1]) {
+            arr.push(strArray[0]);
+            strArray.shift();
+        }
+    } else if (brackets[1] == strArray[0]) {
+      if (brackets[0] == arr[arr.length-1]) {
+        arr.pop();
+        strArray.shift();
+      } else return false;
+    } else if (brackets[0] == strArray[0]) {
+      arr.push(strArray[0]);
+      strArray.shift();
+    }
+  }
+}
+if (arr.length == 0 && strArray.length == 0) {
+    return true
+} else return false;
 }
